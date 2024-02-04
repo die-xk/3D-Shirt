124aae38dddbbe94e2ac8509d7d7296b854e37c3
die-xk <diellonbytyqi14@gmail.com>
2024-02-04 01:46:50 +0100

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Canvas from './canvas';
+import Costumizer from './pages/Costumizer';
+import Home from './pages/Home';
+function App() {
+  return(
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Costumizer />
+    </main>
+  );
+}
+
+export default App
